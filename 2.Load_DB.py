445529a95178f42b3663649f5f90c00def7d24d1
Nick <nicholas.papadimitris@gmail.com>
2025-05-11 00:11:12 +1000

    Update 2.Load_DB.py

--- 2.Load_DB.py
+++ 2.Load_DB.py
@@ -5,14 +5,14 @@ from datetime import datetime
 today = datetime.now().replace(day=1)
 today = today.strftime("%Y-%m-%d")
 
-output_file = f'C:/Users/nicho/PycharmProjects/Projects/API2SQL Pipelines/SWS API Production_V2/SWS_API_Prod_V2/.venv/2.Enhancement/Output/Snowflake_{today}.xlsx'
+output_file = f'C:/***/Output/Snowflake_{today}.xlsx'
 df = pd.read_excel(output_file, engine='openpyxl')
 DB_PARAMS = {
             "host": "localhost",
             "port": "5432",
-            "database": "Simply_API_Prod",
-            "user": "postgres",
-            "password": "Arxidolemios39"}
+            "database": "***",
+            "user": "***",
+            "password": "***"}
 
 engine = create_engine('postgresql+psycopg2://postgres:Arxidolemios39@localhost:5432/Simply_API_Prod')
 
