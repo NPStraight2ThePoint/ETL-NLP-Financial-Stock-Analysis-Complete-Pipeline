1289398f6c976639486b396b5c8fb104903399fa
Nick <nicholas.papadimitris@gmail.com>
2025-05-16 21:41:12 +1000

    Add files via upload

--- dir_utils.py
+++ dir_utils.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 
 FDM = datetime.now().replace(day=1).strftime("%Y-%m-%d")
-BACKUP_DIR = f'C:/..../.venv/Backup'
-output_dir = Path('C:/..../.venv/output')
+BACKUP_DIR = f'C:/Users/nicho/PycharmProjects/Projects/API2SQL Pipelines/SWS API Production_V2/SWS_API_Prod_V2/.venv/Backup'
+output_dir = Path('C:/Users/nicho/PycharmProjects/Projects/API2SQL Pipelines/SWS API Production_V2/SWS_API_Prod_V2/.venv/output')
 
 
 
