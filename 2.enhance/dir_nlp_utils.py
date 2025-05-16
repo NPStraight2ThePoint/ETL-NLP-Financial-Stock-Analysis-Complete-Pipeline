e2d8801517c5bf92e18c7b31cb00122d23d68fff
Nick <nicholas.papadimitris@gmail.com>
2025-05-16 22:44:40 +1000

    Update and rename dir_nlp_utils.py to 2.enhance/dir_nlp_utils.py

--- 2.enhance/dir_nlp_utils.py
+++ 2.enhance/dir_nlp_utils.py
@@ -3,10 +3,10 @@ import os
 from pathlib import Path
 
 FDM = datetime.now().replace(day=1).strftime("%Y-%m-%d")
-BACKUP_DIR = f'C:/Users/nicho/PycharmProjects/Projects/API2SQL Pipelines/SWS API Production_V2/SWS_API_Prod_V2/.venv/Backup'
-output_dir = Path('C:/Users/nicho/PycharmProjects/Projects/API2SQL Pipelines/SWS API Production_V2/SWS_API_Prod_V2/.venv/output')
+BACKUP_DIR = f'C:/.../.venv/Backup'
+output_dir = Path('C:/.../.venv/output')
 base_directories = [
-    'C:/Users/nicho/PycharmProjects/Projects/API2SQL Pipelines/SWS API Production_V2/SWS_API_Prod_V2/.venv/Output']
+    'C:/.../.venv/Output']
 # Define the columns to keep
 columns_to_keep = [
     "ticker", "exchange", "date",
